25e55a408464a2fed47e0ba4580c6f2c39e20fcb
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-05-30 14:45:05 +0900

    Export multitask binary classification model

--- src/bochan/models/classification/binary/base/__init__.py
+++ src/bochan/models/classification/binary/base/__init__.py
@@ -1,8 +1,10 @@
 from .models import BinaryClassificationGPModel, BinaryClassificationMixedGPModel
 from .multioutput import MultiOutputBinaryClassificationModel
+from .multitask import MultiTaskBinaryClassificationGPModel
 
 __all__ = [
     "BinaryClassificationGPModel",
     "BinaryClassificationMixedGPModel",
-    "MultiOutputBinaryClassificationModel"
+    "MultiOutputBinaryClassificationModel",
+    "MultiTaskBinaryClassificationGPModel",
 ]

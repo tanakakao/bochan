baa5ed48ecbc5183aba7c3f4a66f369c4db061a5
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-05-31 18:29:35 +0900

    Expose hybrid prediction helpers

--- src/bochan/models/hybrid/__init__.py
+++ src/bochan/models/hybrid/__init__.py
@@ -1,6 +1,9 @@
 from .multi_output import HybridMultiOutputModel
 from .posterior import HybridPosterior
+from .prediction import attach_prediction_methods
 from .specs import OutputSpec, PosteriorMode, TaskType
+
+attach_prediction_methods(HybridMultiOutputModel)
 
 __all__ = [
     "HybridMultiOutputModel",

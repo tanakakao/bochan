b9b4c013033606f9fd04777b4a49805a3031c740
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-06-07 23:01:02 +0900

    Add multiclass base package exports

---
+++ src/bochan/models/classification/multiclass/base/__init__.py
@@ -0,0 +1,11 @@
+from .models import (
+    MulticlassClassificationGPModel,
+    MulticlassClassificationMixedGPModel,
+    build_mixed_multiclass_kernel,
+)
+
+__all__ = [
+    "MulticlassClassificationGPModel",
+    "MulticlassClassificationMixedGPModel",
+    "build_mixed_multiclass_kernel",
+]

196448efda9685ad733c2fb21522c6388c57b272
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-06-10 08:26:56 +0900

    Export multi-output multiclass model wrapper

--- src/bochan/models/classification/multiclass/base/__init__.py
+++ src/bochan/models/classification/multiclass/base/__init__.py
@@ -3,9 +3,19 @@ from .models import (
     MulticlassClassificationMixedGPModel,
     build_mixed_multiclass_kernel,
 )
+from .multioutput import (
+    MultiOutputMulticlassClassificationGPModel,
+    MultiOutputMulticlassClassificationModel,
+    MultiOutputMulticlassModel,
+    MultiOutputMulticlassProbsPosterior,
+)
 
 __all__ = [
     "MulticlassClassificationGPModel",
     "MulticlassClassificationMixedGPModel",
     "build_mixed_multiclass_kernel",
+    "MultiOutputMulticlassProbsPosterior",
+    "MultiOutputMulticlassClassificationModel",
+    "MultiOutputMulticlassModel",
+    "MultiOutputMulticlassClassificationGPModel",
 ]

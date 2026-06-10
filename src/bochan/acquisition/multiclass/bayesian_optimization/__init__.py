2b804466e0461c90297d40427d1f226d6f59ca5b
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-06-10 17:05:36 +0900

    Use shape-safe multiclass multi-output EHVI export

--- src/bochan/acquisition/multiclass/bayesian_optimization/__init__.py
+++ src/bochan/acquisition/multiclass/bayesian_optimization/__init__.py
@@ -19,7 +19,6 @@ from .multi_output import (
     OutputReductionType,
     compute_observed_multiclass_target_probability_values,
     compute_observed_multiclass_utility,
-    qMultiOutputMulticlassExpectedHypervolumeImprovement,
     qMultiOutputMulticlassExpectedImprovement,
     qMultiOutputMulticlassNParEGO,
     qMultiOutputMulticlassNoisyExpectedHypervolumeImprovement,
@@ -27,6 +26,7 @@ from .multi_output import (
     qMultiOutputMulticlassProbabilityOfImprovement,
     qMultiOutputMulticlassUpperConfidenceBound,
 )
+from .multi_output_ehvi import qMultiOutputMulticlassExpectedHypervolumeImprovement
 from .single_output import (
     compute_multiclass_target_probability_best_f,
     compute_multiclass_target_probability_values,

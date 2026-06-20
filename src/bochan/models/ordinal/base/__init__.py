342c8ab736928257181659d173a0d34766ff9ffd
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-06-21 01:35:10 +0900

    Export Kronecker multitask ordinal model

--- src/bochan/models/ordinal/base/__init__.py
+++ src/bochan/models/ordinal/base/__init__.py
@@ -1,8 +1,10 @@
+from .kronecker_multitask import KroneckerMultiTaskOrdinalGPModel
 from .models import OrdinalGPModel, OrdinalMixedGPModel
 from .multioutput import MultiOutputOrdinalModel
 from .multitask import MultiTaskOrdinalGPModel
 
 __all__ = [
+    "KroneckerMultiTaskOrdinalGPModel",
     "OrdinalGPModel",
     "OrdinalMixedGPModel",
     "MultiOutputOrdinalModel",

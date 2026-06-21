9b438841b3d8e9014e3142fb5c64c04c7daa0a51
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-06-21 18:43:37 +0900

    Export VAE regression model

--- src/bochan/models/regression/gaussian/high_dim/__init__.py
+++ src/bochan/models/regression/gaussian/high_dim/__init__.py
@@ -1,7 +1,18 @@
-from .saas import SaasSingleTaskGP, SaasMixedSingleTaskGP
-from .decomposition import REMBOSingleTaskGP, REMBOMixedSingleTaskGP, PCASingleTaskGP, PCAMixedSingleTaskGP
+from .decomposition import (
+    PCAMixedSingleTaskGP,
+    PCASingleTaskGP,
+    REMBOMixedSingleTaskGP,
+    REMBOSingleTaskGP,
+)
+from .saas import SaasMixedSingleTaskGP, SaasSingleTaskGP
+from .vae import VAESingleTaskGP
 
 __all__ = [
-    "SaasSingleTaskGP", "SaasMixedSingleTaskGP",
-    "REMBOSingleTaskGP", "REMBOMixedSingleTaskGP", "PCASingleTaskGP", "PCAMixedSingleTaskGP"
+    "SaasSingleTaskGP",
+    "SaasMixedSingleTaskGP",
+    "REMBOSingleTaskGP",
+    "REMBOMixedSingleTaskGP",
+    "PCASingleTaskGP",
+    "PCAMixedSingleTaskGP",
+    "VAESingleTaskGP",
 ]

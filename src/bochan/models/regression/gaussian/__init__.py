e9abf8223ed16ffff852b803d4ee39feca871d18
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-06-21 11:01:08 +0900

    Export mixed Gaussian task-feature model

--- src/bochan/models/regression/gaussian/__init__.py
+++ src/bochan/models/regression/gaussian/__init__.py
@@ -2,8 +2,11 @@ from .kronecker_multitask import (
     KroneckerMultiTaskMixedGP,
     MixedKroneckerMultiTaskGP,
 )
+from .multitask import MixedMultiTaskGP, MultiTaskMixedGP
 
 __all__ = [
     "KroneckerMultiTaskMixedGP",
     "MixedKroneckerMultiTaskGP",
+    "MixedMultiTaskGP",
+    "MultiTaskMixedGP",
 ]

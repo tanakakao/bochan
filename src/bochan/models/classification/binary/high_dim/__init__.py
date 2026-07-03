85d918eba96598da30261dfbf6cabf6e44a7b197
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-07-03 10:10:59 +0900

    Install projected binary perturbation compatibility

--- src/bochan/models/classification/binary/high_dim/__init__.py
+++ src/bochan/models/classification/binary/high_dim/__init__.py
@@ -1,5 +1,10 @@
 from .saas import SaasBinaryClassificationGPModel, SaasBinaryClassificationMixedGPModel
 from .decomposition import REMBOBinaryClassificationGPModel, REMBOBinaryClassificationMixedGPModel, PCABinaryClassificationGPModel, PCABinaryClassificationMixedGPModel
+from .input_perturbation_compat import apply_projected_binary_perturbation_compat
+
+
+apply_projected_binary_perturbation_compat()
+
 
 __all__ = [
     "SaasBinaryClassificationGPModel",

dfeb0f3948162fe2bc2d4635439a69b53451ad77
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-07-05 16:07:28 +0900

    patch internal ordinal acquisition classes

--- src/bochan/acquisition/__init__.py
+++ src/bochan/acquisition/__init__.py
@@ -7,16 +7,19 @@ from .classification_constraint_compat import (
     apply_classification_constraint_compat,
 )
 from .nsgaii_constraint_install import install_nsgaii_constraint_compat
+from .ordinal_constraint_install import apply_ordinal_constraint_compat
 from .ordinal_multitask_compat import apply_ordinal_multitask_compat
 
 
 apply_ordinal_multitask_compat()
 apply_classification_constraint_compat()
+apply_ordinal_constraint_compat()
 install_nsgaii_constraint_compat()
 
 
 __all__ = [
     "apply_classification_constraint_compat",
+    "apply_ordinal_constraint_compat",
     "apply_ordinal_multitask_compat",
     "install_nsgaii_constraint_compat",
 ]

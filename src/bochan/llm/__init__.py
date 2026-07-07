a24b60116282573c2659fad93532a94315bf1655
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-07-07 17:59:28 +0900

    add llm model planner

--- src/bochan/llm/__init__.py
+++ src/bochan/llm/__init__.py
@@ -8,6 +8,7 @@ from .candidate_generator import (
 )
 from .client import BaseLLMClient, GeminiClient, LLMResponse, OpenAIClient, make_llm_client
 from .configs import GoalConfig, LLMConfig, LLMContextConfig
+from .planner import build_config_planner_prompt, plan_configs
 from .prompt_builder import build_candidate_prompt, build_goal_planner_prompt
 
 __all__ = [
@@ -19,9 +20,11 @@ __all__ = [
     "LLMResponse",
     "OpenAIClient",
     "build_candidate_prompt",
+    "build_config_planner_prompt",
     "build_goal_planner_prompt",
     "build_llm_candidate_set",
     "make_llm_client",
+    "plan_configs",
     "sanitize_candidate_set",
     "score_candidate_set",
     "select_top_candidates",

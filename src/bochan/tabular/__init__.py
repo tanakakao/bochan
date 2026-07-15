6c9eb4306b82716ee72b4e43b3df15eb57ddfd35
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-07-15 22:22:57 +0900

    Enable output-level category metadata

--- src/bochan/tabular/__init__.py
+++ src/bochan/tabular/__init__.py
@@ -22,10 +22,12 @@ from .converter import (
     resolve_repair_config_columns,
     tensor_to_dataframe,
 )
+from .multi_output_categories import apply_tabular_multi_output_categories
 from .optimizer_api import TabularBayesianOptimizer
 from .ordinal_rank_labels import apply_tabular_ordinal_rank_labels
 from .prediction_labels import apply_tabular_prediction_labels
 
+apply_tabular_multi_output_categories()
 apply_tabular_ordinal_rank_labels()
 apply_tabular_candidate_outputs()
 apply_tabular_prediction_labels()

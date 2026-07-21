14242d9b10cafb543908eb866072bc52d70f4c2e
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-07-21 14:39:45 +0900

    Load feature constraint styles

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 import "./target-settings.css";
+import "./constraint-settings.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>

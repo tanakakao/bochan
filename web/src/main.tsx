736238d3cde8391018af12b54592daae5996c99f
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-07-21 22:06:54 +0900

    Install magnitude-aware number input increments

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { installDynamicNumberInputSteps } from "./numericInputUtils";
 import "./styles.css";
 import "./target-settings.css";
 import "./constraint-settings.css";
 import "./advanced-settings.css";
+
+installDynamicNumberInputSteps();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>

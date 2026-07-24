1febc47cfa226898074e3bf54ad85ee58173d722
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-07-24 19:15:27 +0900

    Load experiment page styles

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -11,6 +11,7 @@ import "./workflow-separation.css";
 import "./ui-adjustments.css";
 import "./model-artifact.css";
 import "./result-interactions.css";
+import "./experiment-results.css";
 
 installDynamicNumberInputSteps();
 

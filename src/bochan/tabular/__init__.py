5a7565dd4f51b9062ef916bf1a55536a8f58ab72
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-08-04 07:24:40 +0900

    Expose variable composition totals

--- src/bochan/tabular/__init__.py
+++ src/bochan/tabular/__init__.py
@@ -38,10 +38,10 @@ from .converter import (
     resolve_repair_config_columns,
     tensor_to_dataframe,
 )
-from .element_column_composition_optimizer import TabularBayesianOptimizer
 from .multi_output_categories import apply_tabular_multi_output_categories
 from .ordinal_rank_labels import apply_tabular_ordinal_rank_labels
 from .prediction_labels import apply_tabular_prediction_labels
+from .variable_total_composition_optimizer import TabularBayesianOptimizer
 
 apply_tabular_multi_output_categories()
 apply_tabular_ordinal_rank_labels()

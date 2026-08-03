3a9315df36a0148c506db6cd1cd3a0844a6d7fd0
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-08-03 23:50:22 +0900

    Fix tabular import ordering

--- src/bochan/tabular/__init__.py
+++ src/bochan/tabular/__init__.py
@@ -27,7 +27,6 @@ from .composition import (
     normalize_composition,
     parse_formula,
 )
-from .element_column_composition_optimizer import TabularBayesianOptimizer
 from .config import ColumnKey, TabularDataConfig, TabularFeatureGroup
 from .converter import (
     TabularDataset,
@@ -39,6 +38,7 @@ from .converter import (
     resolve_repair_config_columns,
     tensor_to_dataframe,
 )
+from .element_column_composition_optimizer import TabularBayesianOptimizer
 from .multi_output_categories import apply_tabular_multi_output_categories
 from .ordinal_rank_labels import apply_tabular_ordinal_rank_labels
 from .prediction_labels import apply_tabular_prediction_labels

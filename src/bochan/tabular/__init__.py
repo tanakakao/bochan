7d952eb170788e4c2b2c4b683fd16d880431dd02
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-08-03 23:25:09 +0900

    Expose multi-site composition optimizer

--- src/bochan/tabular/__init__.py
+++ src/bochan/tabular/__init__.py
@@ -27,7 +27,7 @@ from .composition import (
     normalize_composition,
     parse_formula,
 )
-from .composition_optimizer import TabularBayesianOptimizer
+from .multi_site_composition_optimizer import TabularBayesianOptimizer
 from .config import ColumnKey, TabularDataConfig, TabularFeatureGroup
 from .converter import (
     TabularDataset,

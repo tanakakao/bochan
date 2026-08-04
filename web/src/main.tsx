5ef807ac2677f392246db6457cbbe415ad2c88c1
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-08-04 16:55:22 +0900

    Install composition Web extension

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { installCompositionExtension } from "./compositionExtension";
 import { installDynamicNumberInputSteps } from "./numericInputUtils";
 import "./styles.css";
 import "./target-settings.css";
@@ -15,8 +16,10 @@ import "./experiment-results.css";
 import "./experiment-history.css";
 import "./styles/workbench-design.css";
 import "./data-dropzone.css";
+import "./composition-extension.css";
 
 installDynamicNumberInputSteps();
+installCompositionExtension();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>

d10c07a195a94206a6cf6fa2abd67ae524f03736
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-08-04 16:56:15 +0900

    Use typed composition route module

--- src/bochan/serving/webapp/__init__.py
+++ src/bochan/serving/webapp/__init__.py
@@ -1,13 +1,11 @@
 """FastAPI application used by the React web interface."""
 
-from .composition_web_support import (
-    install_composition_web_support,
-    register_composition_routes,
-)
+from .composition_web_support import install_composition_web_support
 
 install_composition_web_support()
 
 from .app import app, create_app
+from .composition_web_routes import register_composition_routes
 
 register_composition_routes(app)
 

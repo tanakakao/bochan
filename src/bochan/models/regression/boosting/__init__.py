31c96a08b0c598b3fbf754c2e5d899dce6cb59e9
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-08-08 09:15:30 +0900

    Export mixed NGBoost models

--- src/bochan/models/regression/boosting/__init__.py
+++ src/bochan/models/regression/boosting/__init__.py
@@ -1,8 +1,11 @@
 """Tree-based probabilistic boosting surrogate models."""
 
+from .mixed import NGBoostMixedEnsembleModel, NGBoostMixedRegressorModel
 from .ngboost import NGBoostEnsembleModel, NGBoostRegressorModel
 
 __all__ = [
     "NGBoostEnsembleModel",
+    "NGBoostMixedEnsembleModel",
+    "NGBoostMixedRegressorModel",
     "NGBoostRegressorModel",
 ]

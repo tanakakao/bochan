0923eeb69832fdbc07b417a5c95dbba69b384dac
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-08-08 22:36:32 +0900

    Pin PFNs4BO public checkpoints to verified revision

--- src/bochan/models/regression/foundation/__init__.py
+++ src/bochan/models/regression/foundation/__init__.py
@@ -4,7 +4,17 @@ from ._pfns4bo_compat import apply_pfns4bo_torch_compat
 
 apply_pfns4bo_torch_compat()
 
-from .pfn import PFNPosterior, PFNRegressorModel, load_pfns4bo_pretrained  # noqa: E402
+from . import pfn as _pfn  # noqa: E402
+
+_PFNS4BO_CHECKPOINT_REVISION = "4c0d901e4e0f1d5afd4b12e33974525cf3493dd3"
+_pfn._PUBLIC_MODEL_BASE_URL = (
+    "https://github.com/automl/PFNs4BO/raw/"
+    f"{_PFNS4BO_CHECKPOINT_REVISION}/pfns4bo/final_models"
+)
+
+PFNPosterior = _pfn.PFNPosterior
+PFNRegressorModel = _pfn.PFNRegressorModel
+load_pfns4bo_pretrained = _pfn.load_pfns4bo_pretrained
 
 __all__ = [
     "PFNPosterior",

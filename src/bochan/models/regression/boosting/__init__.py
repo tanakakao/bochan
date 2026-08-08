e7461820b482caf4db8318f1575381de3e242054
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-08-08 10:42:51 +0900

    Export Random Forest surrogate models

--- src/bochan/models/regression/boosting/__init__.py
+++ src/bochan/models/regression/boosting/__init__.py
@@ -1,11 +1,14 @@
-"""Tree-based probabilistic boosting surrogate models."""
+"""Tree-based surrogate models."""
 
 from .mixed import NGBoostMixedEnsembleModel, NGBoostMixedRegressorModel
 from .ngboost import NGBoostEnsembleModel, NGBoostRegressorModel
+from .random_forest import RandomForestMixedRegressorModel, RandomForestRegressorModel
 
 __all__ = [
     "NGBoostEnsembleModel",
     "NGBoostMixedEnsembleModel",
     "NGBoostMixedRegressorModel",
     "NGBoostRegressorModel",
+    "RandomForestMixedRegressorModel",
+    "RandomForestRegressorModel",
 ]

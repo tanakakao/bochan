46cd00741d875e08c89b68a0cdaeec52007ec224
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-08-08 20:48:54 +0900

    Add neural ordinal package

---
+++ src/bochan/models/ordinal/neural/__init__.py
@@ -0,0 +1,11 @@
+"""Neural ensemble ordinal models."""
+
+from .deep_ensemble import (
+    DeepEnsembleMixedOrdinalModel,
+    DeepEnsembleOrdinalModel,
+)
+
+__all__ = [
+    "DeepEnsembleMixedOrdinalModel",
+    "DeepEnsembleOrdinalModel",
+]

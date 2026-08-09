795fd9a8e52241835aa6bf661dcf77dcf3d0bf1c
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-08-09 13:52:46 +0900

    Export TabPFN regression models

--- src/bochan/models/regression/foundation/__init__.py
+++ src/bochan/models/regression/foundation/__init__.py
@@ -5,6 +5,7 @@ from ._pfns4bo_compat import apply_pfns4bo_torch_compat
 apply_pfns4bo_torch_compat()
 
 from . import pfn as _pfn  # noqa: E402
+from .tabpfn import TabPFNMixedRegressorModel, TabPFNRegressorModel  # noqa: E402
 
 _PFNS4BO_CHECKPOINT_REVISION = "4c0d901e4e0f1d5afd4b12e33974525cf3493dd3"
 _pfn._PUBLIC_MODEL_BASE_URL = (
@@ -19,5 +20,7 @@ load_pfns4bo_pretrained = _pfn.load_pfns4bo_pretrained
 __all__ = [
     "PFNPosterior",
     "PFNRegressorModel",
+    "TabPFNMixedRegressorModel",
+    "TabPFNRegressorModel",
     "load_pfns4bo_pretrained",
 ]

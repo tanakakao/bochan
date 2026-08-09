b8d83fb22c082c6995c690a7b75e4bd59b8310ef
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-08-09 09:31:56 +0900

    Export LightGBM regression models

--- src/bochan/models/regression/external/__init__.py
+++ src/bochan/models/regression/external/__init__.py
@@ -1,5 +1,11 @@
 """External-estimator regression surrogate models."""
 
+from .lightgbm import (
+    LightGBMEnsembleModel,
+    LightGBMMixedEnsembleModel,
+    LightGBMMixedRegressorModel,
+    LightGBMRegressorModel,
+)
 from .ngboost import (
     NGBoostEnsembleModel,
     NGBoostMixedEnsembleModel,
@@ -9,6 +15,10 @@ from .ngboost import (
 from .random_forest import RandomForestMixedRegressorModel, RandomForestRegressorModel
 
 __all__ = [
+    "LightGBMEnsembleModel",
+    "LightGBMMixedEnsembleModel",
+    "LightGBMMixedRegressorModel",
+    "LightGBMRegressorModel",
     "NGBoostEnsembleModel",
     "NGBoostMixedEnsembleModel",
     "NGBoostMixedRegressorModel",

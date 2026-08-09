9f3e17b699aced2b24201b177c05454dadf7e3f1
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-08-10 02:01:16 +0900

    Route ordinal active learning through nominal duplicate handling

--- src/bochan/acquisition/ordinal/active_learning/__init__.py
+++ src/bochan/acquisition/ordinal/active_learning/__init__.py
@@ -1,25 +1,20 @@
-from .bald import qMultiOutputOrdinalBALD
-from .hetero_multi_output import (
+from .nominal_duplicate_safe import (
     qHeteroMultiOutputOrdinalBALD,
     qHeteroMultiOutputOrdinalIntegratedPosteriorVarianceProxy,
     qHeteroMultiOutputOrdinalMarginUncertainty,
     qHeteroMultiOutputOrdinalPredictiveEntropy,
     qHeteroMultiOutputOrdinalUtilityVariance,
-)
-from .hetero_single_output import (
     qHeteroOrdinalBALD,
     qHeteroOrdinalIntegratedPosteriorVariance,
     qHeteroOrdinalMarginUncertainty,
     qHeteroOrdinalPredictiveEntropy,
     qHeteroOrdinalUtilityVariance,
-)
-from .multi_output import (
+    qMultiOutputOrdinalBALD,
     qMultiOutputOrdinalFantasyNegIntegratedPosteriorVariance,
+    qMultiOutputOrdinalIntegratedPosteriorVarianceProxy,
     qMultiOutputOrdinalMarginUncertainty,
     qMultiOutputOrdinalPredictiveEntropy,
     qMultiOutputOrdinalUtilityVariance,
-)
-from .single_output import (
     qOrdinalBALD,
     qOrdinalFantasyNegIntegratedPosteriorVariance,
     qOrdinalMarginUncertainty,

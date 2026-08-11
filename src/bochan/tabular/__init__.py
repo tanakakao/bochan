542260a5e9366b2b077a39f1a2eccf66ca767d74
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-08-11 23:54:57 +0900

    Make tabular package declarative

--- src/bochan/tabular/__init__.py
+++ src/bochan/tabular/__init__.py
@@ -26,7 +26,6 @@ from .composition import (
     normalize_composition,
     parse_formula,
 )
-from .composition_bounds_optimizer import TabularBayesianOptimizer
 from .config import ColumnKey, TabularDataConfig, TabularFeatureGroup
 from .converter import (
     TabularDataset,
@@ -38,18 +37,12 @@ from .converter import (
     resolve_repair_config_columns,
     tensor_to_dataframe,
 )
-from .multi_output_categories import apply_tabular_multi_output_categories
 from .observation_data import (
     ObservationTabularDataset,
     dataframe_to_observation_tensors,
     numpy_to_observation_tensors,
 )
-from .ordinal_rank_labels import apply_tabular_ordinal_rank_labels
-from .prediction_labels import apply_tabular_prediction_labels
-
-apply_tabular_multi_output_categories()
-apply_tabular_ordinal_rank_labels()
-apply_tabular_prediction_labels()
+from .public_optimizer import TabularBayesianOptimizer
 
 __all__ = [
     "ATOMIC_NUMBERS",

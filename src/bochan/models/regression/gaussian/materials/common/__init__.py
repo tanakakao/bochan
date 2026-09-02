1c38d6dbd136de9eb64e025f6c84a5b7140b25ef
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-09-03 08:24:29 +0900

    refactor(materials): export encoder training policy

--- src/bochan/models/regression/gaussian/materials/common/__init__.py
+++ src/bochan/models/regression/gaussian/materials/common/__init__.py
@@ -6,10 +6,24 @@ from .fusion import (
     MaterialProcessFusion,
     build_material_process_fusion,
 )
+from .training import (
+    EncoderTrainingMode,
+    EncoderTrainingPolicy,
+    apply_encoder_train_mode,
+    apply_encoder_training_policy,
+    configure_encoder_parameters,
+    unique_module_parameters,
+)
 
 __all__ = [
     "ConcatFusion",
+    "EncoderTrainingMode",
+    "EncoderTrainingPolicy",
     "MaterialEncoder",
     "MaterialProcessFusion",
+    "apply_encoder_train_mode",
+    "apply_encoder_training_policy",
     "build_material_process_fusion",
+    "configure_encoder_parameters",
+    "unique_module_parameters",
 ]

d2822044bc6f153e1e3dbbab6c3ef098d63eeb07
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-09-03 08:14:24 +0900

    refactor(materials): add canonical CHGNet namespace

---
+++ src/bochan/models/regression/gaussian/materials/structure/chgnet.py
@@ -0,0 +1,25 @@
+"""Canonical CHGNet Gaussian-model import surface."""
+
+from ...deep.chgnet import (
+    CHGNetDKLModel,
+    CHGNetGPModel,
+    CHGNetMixedDKLModel,
+    CHGNetMixedGPModel,
+)
+from ...deep.chgnet_multitask import (
+    CHGNetMixedMultiTaskDKLModel,
+    CHGNetMixedMultiTaskGPModel,
+    CHGNetMultiTaskDKLModel,
+    CHGNetMultiTaskGPModel,
+)
+
+__all__ = [
+    "CHGNetDKLModel",
+    "CHGNetGPModel",
+    "CHGNetMixedDKLModel",
+    "CHGNetMixedGPModel",
+    "CHGNetMixedMultiTaskDKLModel",
+    "CHGNetMixedMultiTaskGPModel",
+    "CHGNetMultiTaskDKLModel",
+    "CHGNetMultiTaskGPModel",
+]
